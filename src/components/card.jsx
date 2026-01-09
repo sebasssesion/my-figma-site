0d2cd9cb65fb19be6752825c6ea0d8e5944d5965
Sebastian <sebastian@MacBookPro.home>
2026-01-09 15:37:38 +0200

    Final Layout

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,27 +4,32 @@ function Card({ children }) {
       <div className="card">
         <div className="card-top">
           {/* Left: BTC badge */}
-          <div className="card-left">
-            {children}
-          </div>
-
-          {/* Right: values */}
-          <div className="card-right">
-            {/* Top value */}
-            <div className="card-value">0.89</div>
-
-            {/* Bottom row: 1% + price */}
-            <div className="card-bottom-row">
-              <span className="card-change">1%</span>
-
-              <span className="card-price">
-                <span className="currency">$</span>
-                <span className="amount">650.00</span>
-              </span>
+        <div className="card-left">
+            <div className="card-left-inner">
+                {children}
             </div>
-          </div>
         </div>
-      </div>
+
+
+          {/* Right side */}
+          <div className="card-right">
+            <div className="card-right-inner">
+              <div className="card-value">0.89</div>
+
+              <div className="card-bottom-inner">
+                <div className="card-bottom-row">
+                  <span className="card-change">1%</span>
+
+                  <span className="card-price">
+                    <span className="currency">$</span>
+                    <span className="amount">650.00</span>
+                  </span>
+                </div>
+              </div>
+            </div> {/* ✅ closes card-right-inner */}
+          </div> {/* closes card-right */}
+        </div> {/* closes card-top */}
+      </div> {/* closes card */}
     </div>
   );
 }
