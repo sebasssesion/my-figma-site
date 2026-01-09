7502503426a897163e3d4a67fae92b9bc0a4c18b
sebasssesion <sebasssesion@gmail.com>
2026-01-09 13:24:40 +0200

    Card layout and text allignment

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,24 @@ function Card({ children }) {
   return (
     <div className="card-border">
       <div className="card">
-        {children}
+        <div className="card-top">
+          <div className="card-left">
+            {children}
+          </div>
+
+          <div className="card-right">
+            {/* Big number */}
+            <div className="card-value">0.89</div>
+
+            {/* Bottom row: 1% + $650.00 */}
+            <div className="card-price-container">
+              <span className="card-change">1%</span>
+              <span className="card-price">$650.00</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Remove the old card-bottom div since price is now in card-right */}
       </div>
     </div>
   );
